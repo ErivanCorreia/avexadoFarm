3776c0811ae4e5e476631133da3e5feced617a3d
ErivanCorreia <erivann47@gmail.com>
2020-01-18 11:59:19 -0300

    :tada: initial commit

---
+++ src/main/java/br/com/avexadoFarm/AvexadoFarmApplication.java
@@ -0,0 +1,13 @@
+package br.com.avexadoFarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvexadoFarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvexadoFarmApplication.class, args);
+	}
+
+}
