cb526aa177a77245bc3e32f270f3905e7d2f7e01
ErivanCorreia <erivann47@gmail.com>
2020-03-05 21:26:23 -0300

    :construction: Criacao de servicos de infraestrutura

---
+++ src/main/java/br/com/avexadoFarm/infrastructure/service/ConverterService.java
@@ -0,0 +1,22 @@
+package br.com.avexadoFarm.infrastructure.service;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.ObjectError;
+
+@Service
+public class ConverterService {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public <T> T converter(Object origem, Class<T> destino) {
+        return modelMapper.map(origem, destino);
+    }
+
+    public <T> Page<T> converter(Page<?> origem, Class<T> destino) {
+        return origem.map(o -> modelMapper.map(o, destino));
+    }
+}
