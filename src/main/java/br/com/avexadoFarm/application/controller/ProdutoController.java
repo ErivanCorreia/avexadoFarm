7a9e0ace1b86008db3e2ad44f5cca22660200f83
ErivanCorreia <erivann47@gmail.com>
2020-03-05 21:30:12 -0300

    :construction: Criacao de controller

---
+++ src/main/java/br/com/avexadoFarm/application/controller/ProdutoController.java
@@ -0,0 +1,40 @@
+package br.com.avexadoFarm.application.controller;
+
+import br.com.avexadoFarm.application.service.ProdutoServiceImpl;
+import br.com.avexadoFarm.domain.model.Produto;
+import br.com.avexadoFarm.infrastructure.service.ConverterService;
+import br.com.avexadoFarm.presentation.ProdutoRequestDTO;
+import br.com.avexadoFarm.presentation.ProdutoResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+    @Autowired
+    private ProdutoServiceImpl produtoServiceImpl;
+
+    @Autowired
+    private ConverterService converterService;
+
+    @GetMapping
+    public ResponseEntity<Page<ProdutoResponseDTO>> findAll(Pageable pageable) {
+        Page<Produto> produtos = produtoServiceImpl.findAll(pageable);
+        Page<ProdutoResponseDTO> produtoResponseDTOS = converterService.
+                converter(produtos, ProdutoResponseDTO.class);
+        return ResponseEntity.ok(produtoResponseDTOS);
+    }
+
+    @PostMapping
+    public ResponseEntity<ProdutoResponseDTO> save(@RequestBody ProdutoRequestDTO produtoRequestDTO) {
+        Produto produto = converterService.converter(produtoRequestDTO, Produto.class);
+        Produto produtoSalvo = produtoServiceImpl.save(produto);
+        ProdutoResponseDTO produtoResponseDTO = converterService.
+                converter(produtoSalvo, ProdutoResponseDTO.class);
+        return ResponseEntity.ok(produtoResponseDTO);
+    }
+}
