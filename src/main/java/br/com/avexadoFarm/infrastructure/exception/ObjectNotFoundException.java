2ead96b7b0773cd247904c78a5b22e3917f55494
ErivanCorreia <erivann47@gmail.com>
2020-03-05 22:12:02 -0300

    :construction: Cria excecao personalizada

---
+++ src/main/java/br/com/avexadoFarm/infrastructure/exception/ObjectNotFoundException.java
@@ -0,0 +1,7 @@
+package br.com.avexadoFarm.infrastructure.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+    public ObjectNotFoundException(String mensagem) { super(mensagem); }
+
+}
