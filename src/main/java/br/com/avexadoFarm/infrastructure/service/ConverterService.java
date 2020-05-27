20393d8bfb3223197c6cec4d25e642128c40fdc5
ErivanCorreia <erivann47@gmail.com>
2020-05-27 20:17:25 -0300

    Remove import

--- src/main/java/br/com/avexadoFarm/infrastructure/service/ConverterService.java
+++ src/main/java/br/com/avexadoFarm/infrastructure/service/ConverterService.java
@@ -4,7 +4,6 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
-import org.springframework.validation.ObjectError;
 
 @Service
 public class ConverterService {
