b1fc3f86dfa261f55fff58ea46470bd1433f0556
ErivanCorreia <erivann47@gmail.com>
2020-06-11 13:02:26 -0300

    :art: Melhora a estrutura de excecao

--- src/main/java/br/com/avexadoFarm/application/handler/ApplicationExceptionHandler.java
+++ src/main/java/br/com/avexadoFarm/application/handler/ApplicationExceptionHandler.java
@@ -1,16 +1,21 @@
 package br.com.avexadoFarm.application.handler;
 
 import br.com.avexadoFarm.infrastructure.exception.ObjectNotFoundException;
+import br.com.avexadoFarm.infrastructure.exception.StandardError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class ApplicationExceptionHandler {
 
     @ExceptionHandler(ObjectNotFoundException.class)
-    public ResponseEntity<?> referenciaNaoEncontradaException(ObjectNotFoundException exception) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    public ResponseEntity<StandardError> ObjectNotFound(ObjectNotFoundException exception, WebRequest request) {
+        StandardError error = new StandardError(HttpStatus.NOT_FOUND.value(),
+                exception.getMessage(), request.getDescription(false).substring(4));
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 }
