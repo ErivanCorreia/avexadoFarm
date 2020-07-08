cbb1cc5d3c804b003a9fb61cc68f11ea43d7989a
ErivanCorreia <erivann47@gmail.com>
2020-07-07 21:32:48 -0300

    :construction: Ajusta local de Configuracoes de seguranca

--- src/main/java/br/com/avexadoFarm/infrastructure/security/configuration/SecurityConfig.java
+++ src/main/java/br/com/avexadoFarm/infrastructure/security/configuration/SecurityConfig.java
@@ -1,4 +1,4 @@
-package br.com.avexadoFarm.application.configuration;
+package br.com.avexadoFarm.infrastructure.security.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
