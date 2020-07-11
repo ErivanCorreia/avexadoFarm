b00ee909415af84ea8c55f95d3104da491e5af5d
ErivanCorreia <erivann47@gmail.com>
2020-07-11 13:48:24 -0300

    Adiciona configuracoes globais de aplicacao

--- src/main/java/br/com/avexadoFarm/application/configuration/ApplicationConfigurationProperties.java
+++ src/main/java/br/com/avexadoFarm/application/configuration/ApplicationConfigurationProperties.java
@@ -1,7 +1,38 @@
 package br.com.avexadoFarm.application.configuration;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
-@Configuration
-public class ConfigurationPropperties {
+@Validated
+@Component
+@ConfigurationProperties("avexado-farm")
+public class ApplicationConfigurationProperties {
+
+    private JwtToken jwtToken = new JwtToken();
+
+    public JwtToken getJwtToken() {
+        return jwtToken;
+    }
+
+    public static class JwtToken {
+        private String segredo;
+        private Long expriracao;
+
+        public String getSegredo() {
+            return segredo;
+        }
+
+        public void setSegredo(String segredo) {
+            this.segredo = segredo;
+        }
+
+        public Long getExpriracao() {
+            return expriracao;
+        }
+
+        public void setExpriracao(Long expriracao) {
+            this.expriracao = expriracao;
+        }
+    }
 }
