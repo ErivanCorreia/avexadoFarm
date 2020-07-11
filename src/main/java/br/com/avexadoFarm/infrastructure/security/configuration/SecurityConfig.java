84ef46fbf43ff1727fb9c81b231aaa29950125e9
ErivanCorreia <erivann47@gmail.com>
2020-07-11 13:51:43 -0300

    Cria metodo 'configure' para acessos de endpoint

--- src/main/java/br/com/avexadoFarm/infrastructure/security/configuration/SecurityConfig.java
+++ src/main/java/br/com/avexadoFarm/infrastructure/security/configuration/SecurityConfig.java
@@ -2,10 +2,24 @@ package br.com.avexadoFarm.infrastructure.security.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String[] PUBLIC_MATCHERS = {
+            "/produtos"
+    };
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .authorizeRequests()
+                .antMatchers(PUBLIC_MATCHERS).permitAll();
+        // TODO: 11/07/2020 Adicionar filtro (implementar classe responsável por filtro)
+    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
