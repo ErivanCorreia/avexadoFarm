22d9faad7cf7ad1e436284b9b10d56995d168caa
ErivanCorreia <erivann47@gmail.com>
2020-07-11 13:50:21 -0300

    Cria JWTUtil

--- src/main/java/br/com/avexadoFarm/infrastructure/security/util/JWTUtil.java
+++ src/main/java/br/com/avexadoFarm/infrastructure/security/util/JWTUtil.java
@@ -1,4 +1,31 @@
 package br.com.avexadoFarm.infrastructure.security.util;
 
+
+import br.com.avexadoFarm.application.configuration.ApplicationConfigurationProperties;
+import br.com.avexadoFarm.application.configuration.ApplicationConfigurationProperties.JwtToken;
+import br.com.avexadoFarm.infrastructure.security.UserDetail;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
 public class JWTUtil {
+
+    private ApplicationConfigurationProperties applicationConfigurationProperties;
+
+    public String generateToken(UserDetail userDetail){
+
+        return Jwts.builder()
+                .setSubject(userDetail.getUsername())
+                .setExpiration(new Date(System.currentTimeMillis() + getJwtToken().getExpriracao()))
+                .signWith(SignatureAlgorithm.HS512, getJwtToken().getSegredo())
+                .compact();
+    }
+
+    public JwtToken getJwtToken() {
+        return applicationConfigurationProperties.getJwtToken();
+    }
+
 }
