fdf468b977c8e2aba8e01b2285fe9a9bfdf89919
ErivanCorreia <erivann47@gmail.com>
2020-08-01 16:48:58 -0300

    :construction: Modifica 'Filte' -> 'BasicAuthenticationFilter' && ajusta injecao de dependencia

--- src/main/java/br/com/avexadoFarm/infrastructure/security/filter/JWTAuthenticationFilter.java
+++ src/main/java/br/com/avexadoFarm/infrastructure/security/filter/JWTAuthenticationFilter.java
@@ -4,8 +4,11 @@ import br.com.avexadoFarm.infrastructure.security.UserDetail;
 import br.com.avexadoFarm.infrastructure.security.service.UserDetailServiceImpl;
 import br.com.avexadoFarm.infrastructure.security.util.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import javax.servlet.Filter;
 import javax.servlet.ServletRequest;
@@ -13,36 +16,38 @@ import javax.servlet.ServletResponse;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
 import java.io.IOException;
 
-public class JWTAuthenticationFilter implements Filter {
+public class JWTAuthenticationFilter extends BasicAuthenticationFilter {
 
-    @Autowired
     private JWTUtil jwtUtil;
 
-    @Autowired
     private UserDetailServiceImpl userDetailService;
 
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+    public JWTAuthenticationFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil, UserDetailServiceImpl userDetailsService) {
+        super(authenticationManager);
+        this.jwtUtil = jwtUtil;
+        this.userDetailService = userDetailsService;
+    }
 
-        String token = httpServletRequest.getHeader(jwtUtil.AUTH);
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String token = request.getHeader(jwtUtil.AUTH);
         if (token != null && token.startsWith(jwtUtil.BEARER)) {
             token = token.substring(7);
         }
 
         String username = jwtUtil.getUsername(token);
 
-        if (username != null && jwtUtil.validateDateExpitation(username)){
-            UsernamePasswordAuthenticationToken auth = getAuthentication(token);
+        if (username != null && jwtUtil.validateDateExpitation(token)){
+            UsernamePasswordAuthenticationToken auth = getAuthentication(username);
             SecurityContextHolder.getContext().setAuthentication(auth);
         }
 
-        chain.doFilter(httpServletRequest, response);
-
+        chain.doFilter(request, response);
     }
 
     private UsernamePasswordAuthenticationToken getAuthentication(String username) {
