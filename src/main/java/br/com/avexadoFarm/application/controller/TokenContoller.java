d106a8217c04002de432b130269df5d0e9b17eb9
ErivanCorreia <erivann47@gmail.com>
2020-08-01 16:44:13 -0300

    :art: Cria Controller para criacao de token

--- src/main/java/br/com/avexadoFarm/application/controller/TokenContoller.java
+++ src/main/java/br/com/avexadoFarm/application/controller/TokenContoller.java
@@ -1,4 +1,24 @@
 package br.com.avexadoFarm.application.controller;
 
+import br.com.avexadoFarm.infrastructure.security.service.AuthenticationServiceImpl;
+import br.com.avexadoFarm.presentation.dto.usuario.UsuarioAuthenticationDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.security.sasl.AuthenticationException;
+
+@RestController
+@RequestMapping("/tokens")
 public class TokenContoller {
+
+    @Autowired
+    private AuthenticationServiceImpl authenticationService;
+
+    @PostMapping
+    public ResponseEntity<String> autenticar(
+            @RequestBody UsuarioAuthenticationDTO usuarioAuthenticationDTO) {
+        String token = authenticationService.authenticate(usuarioAuthenticationDTO);
+        return ResponseEntity.ok(token);
+    }
 }
