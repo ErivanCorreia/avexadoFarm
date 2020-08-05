71b42dc9ae7880a50afd282f58edf969bdcffb55
ErivanCorreia <erivann47@gmail.com>
2020-08-05 20:46:20 -0300

    :fire: Remove variavel sem uso

--- src/main/java/br/com/avexadoFarm/application/configuration/ApplicationConfigurationProperties.java
+++ src/main/java/br/com/avexadoFarm/application/configuration/ApplicationConfigurationProperties.java
@@ -9,7 +9,6 @@ public class ApplicationConfigurationProperties {
 
     private String segredo;
     private Long expiracao;
-    private PropiedadeProperties propriedade = new PropiedadeProperties();
 
     public String getSegredo() {
         return segredo;
@@ -27,32 +26,4 @@ public class ApplicationConfigurationProperties {
         this.expiracao = expiracao;
     }
 
-    public PropiedadeProperties getPropriedade() {
-        return propriedade;
-    }
-
-    public void setPropriedade(PropiedadeProperties propriedade) {
-        this.propriedade = propriedade;
-    }
-
-    public static class PropiedadeProperties {
-        private String nada;
-        private int numero;
-
-        public String getNada() {
-            return nada;
-        }
-
-        public void setNada(String nada) {
-            this.nada = nada;
-        }
-
-        public int getNumero() {
-            return numero;
-        }
-
-        public void setNumero(int numero) {
-            this.numero = numero;
-        }
-    }
 }
