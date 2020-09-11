a74c66105e28e7d07d9ec3e448b7b6edd76536a6
ErivanCorreia <erivann47@gmail.com>
2020-09-11 11:43:18 -0300

    :construction: Remove destinatario

--- src/main/java/br/com/avexadoFarm/application/service/EmailServiceImpl.java
+++ src/main/java/br/com/avexadoFarm/application/service/EmailServiceImpl.java
@@ -1,5 +1,6 @@
 package br.com.avexadoFarm.application.service;
 
+import br.com.avexadoFarm.application.configuration.ApplicationConfigurationProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -11,9 +12,11 @@ public class EmailServiceImpl {
     @Autowired
     private JavaMailSender javaMailSender;
 
+    @Autowired
+    private ApplicationConfigurationProperties properties;
+
     public void sendSimpleMessage(String destinatario, String assunto, String mensagem) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("erivann47@gmail.com");
         message.setSubject(assunto);
         message.setText(mensagem);
         message.setTo(destinatario);
