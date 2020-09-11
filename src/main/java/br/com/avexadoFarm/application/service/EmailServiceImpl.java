f2ffc742b00467ebf30ee7dd8de52a944132565b
ErivanCorreia <erivann47@gmail.com>
2020-09-11 10:43:35 -0300

    :construction: Implementa componente de envio de e-mail

--- src/main/java/br/com/avexadoFarm/application/service/EmailServiceImpl.java
+++ src/main/java/br/com/avexadoFarm/application/service/EmailServiceImpl.java
@@ -1,4 +1,25 @@
 package br.com.avexadoFarm.application.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Component
 public class EmailServiceImpl {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void sendSimpleMessage(String destinatario, String assunto, String mensagem) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("erivann47@gmail.com");
+        message.setSubject(assunto);
+        message.setText(mensagem);
+        message.setTo(destinatario);
+
+        javaMailSender.send(message);
+    }
+
+
 }
