5d72e697fac199047cbaf6942619738e0b329a75
ErivanCorreia <erivann47@gmail.com>
2020-09-14 20:14:49 -0300

    :art: Cria DTO para e-mail

--- src/main/java/br/com/avexadoFarm/presentation/dto/email/EmailResquestDTO.java
+++ src/main/java/br/com/avexadoFarm/presentation/dto/email/EmailResquestDTO.java
@@ -1,4 +1,41 @@
 package br.com.avexadoFarm.presentation.dto.email;
 
 public class EmailResquestDTO {
+
+    private String assunto;
+    private String menssagem;
+    private String destinatario;
+
+    public EmailResquestDTO() {
+    }
+
+    public EmailResquestDTO(String assunto, String menssagem, String destinatario) {
+        this.assunto = assunto;
+        this.menssagem = menssagem;
+        this.destinatario = destinatario;
+    }
+
+    public String getAssunto() {
+        return assunto;
+    }
+
+    public void setAssunto(String assunto) {
+        this.assunto = assunto;
+    }
+
+    public String getMenssagem() {
+        return menssagem;
+    }
+
+    public void setMenssagem(String menssagem) {
+        this.menssagem = menssagem;
+    }
+
+    public String getDestinatario() {
+        return destinatario;
+    }
+
+    public void setDestinatario(String destinatario) {
+        this.destinatario = destinatario;
+    }
 }
