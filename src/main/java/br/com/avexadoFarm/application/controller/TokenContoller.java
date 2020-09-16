76522be0af8d0680bd6225dc82b379774c5acc97
ErivanCorreia <erivann47@gmail.com>
2020-09-16 09:04:17 -0300

    :fire: Ajusta imports

--- src/main/java/br/com/avexadoFarm/application/controller/TokenContoller.java
+++ src/main/java/br/com/avexadoFarm/application/controller/TokenContoller.java
@@ -4,9 +4,10 @@ import br.com.avexadoFarm.infrastructure.security.service.AuthenticationServiceI
 import br.com.avexadoFarm.presentation.dto.usuario.UsuarioAuthenticationDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import javax.security.sasl.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/tokens")
