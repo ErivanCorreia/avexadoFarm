751606d124ff65e7b6ee66854a4993762c553056
ErivanCorreia <erivann47@gmail.com>
2020-09-18 16:33:37 -0300

    :art: Define Autorizacao para acessar endpoint

--- src/main/java/br/com/avexadoFarm/application/controller/UsuarioController.java
+++ src/main/java/br/com/avexadoFarm/application/controller/UsuarioController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -22,6 +23,7 @@ public class UsuarioController {
     private ConverterService converterService;
 
     @GetMapping
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public ResponseEntity<Page<UsuarioResponseDTO>> findAll(Pageable pePageable) {
         Page<Usuario> usuarios = usuarioService.findAll(pePageable);
         Page<UsuarioResponseDTO> usuarioResponseDTOS = converterService.converter(usuarios, UsuarioResponseDTO.class);
@@ -30,6 +32,7 @@ public class UsuarioController {
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public ResponseEntity<UsuarioResponseDTO> findById(@PathVariable Long id) {
         Usuario usuario = usuarioService.findById(id);
         UsuarioResponseDTO usuarioResponseDTO = converterService.converter(usuario, UsuarioResponseDTO.class);
@@ -37,6 +40,7 @@ public class UsuarioController {
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public ResponseEntity<UsuarioResponseDTO> save(@RequestBody UsuarioRequestDTO usuarioRequestDTO) {
         Usuario usuario = converterService.converter(usuarioRequestDTO, Usuario.class);
         Usuario usuarioSalvo = usuarioService.save(usuario);
